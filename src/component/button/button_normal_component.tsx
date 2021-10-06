ef78201ba2d076161e9e34de81b8e4fdf1ec18ec
Imron Rosdiana <imron02@gmail.com>
2021-10-06 12:53:10 +0700

    refactor: support validation

--- src/component/button/button_normal_component.tsx
+++ src/component/button/button_normal_component.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button } from 'react-native-paper';
+import { ColorEnum } from 'utils/colors';
 
 interface ButtonNormalComponentProps {
   label: string;
   onPress: () => void;
   accessibilityLabel?: string;
+  disabled?: boolean;
 }
 
 export default function ButtonNormalComponent(props: ButtonNormalComponentProps) {
@@ -13,7 +15,10 @@ export default function ButtonNormalComponent(props: ButtonNormalComponentProps)
       mode="contained"
       onPress={props.onPress}
       contentStyle={{ height: 50 }}
-      accessibilityLabel={props.accessibilityLabel}>
+      accessibilityLabel={props.accessibilityLabel}
+      style={{ backgroundColor: props.disabled ? ColorEnum.grey100 : ColorEnum.red }}
+      labelStyle={{ color: props.disabled ? ColorEnum.grey200 : ColorEnum.white }}
+      disabled={props.disabled}>
       {props.label}
     </Button>
   );
