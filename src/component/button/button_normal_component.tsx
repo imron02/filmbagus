4f68a7f5585a01fe194eafceaa8417db364e4361
Imron Rosdiana <imron02@gmail.com>
2021-10-21 22:22:15 +0700

    feat: add debounce and async storage

--- src/component/button/button_normal_component.tsx
+++ src/component/button/button_normal_component.tsx
@@ -1,8 +1,10 @@
+import { useDebouncedCallback } from 'use-debounce';
 import React from 'react';
 import { Button } from 'react-native-paper';
 import { ColorEnum } from 'utils/colors';
 
 interface ButtonNormalComponentProps {
+  loading?: boolean;
   label: string;
   onPress: () => void;
   accessibilityLabel?: string;
@@ -10,15 +12,18 @@ interface ButtonNormalComponentProps {
 }
 
 export default function ButtonNormalComponent(props: ButtonNormalComponentProps) {
+  const pressDebounced = useDebouncedCallback(props.onPress, 500);
+
   return (
     <Button
       mode="contained"
-      onPress={props.onPress}
+      onPress={pressDebounced}
       contentStyle={{ height: 50 }}
       accessibilityLabel={props.accessibilityLabel}
       style={{ backgroundColor: props.disabled ? ColorEnum.grey100 : ColorEnum.red }}
       labelStyle={{ color: props.disabled ? ColorEnum.grey200 : ColorEnum.white }}
-      disabled={props.disabled}>
+      disabled={props.disabled}
+      loading={props.loading}>
       {props.label}
     </Button>
   );
