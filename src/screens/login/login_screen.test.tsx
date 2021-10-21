333c39cace9d3b08d80b9965164f58b6fb51ef51
Imron Rosdiana <imron02@gmail.com>
2021-10-21 23:07:43 +0700

    test: add flow login test

--- src/screens/login/login_screen.test.tsx
+++ src/screens/login/login_screen.test.tsx
@@ -1,9 +1,21 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render, waitFor } from '@testing-library/react-native';
 import LoginScreen from './login_screen';
+import AuthContext from 'context/auth_context';
 
-const setup = () => render(<LoginScreen />);
+const mockContext = {
+  userToken: null,
+  signIn: jest.fn(() => Promise.resolve()),
+  signOut: jest.fn(() => Promise.resolve()),
+  isLoading: false
+};
+const setup = () =>
+  render(
+    <AuthContext.Provider value={mockContext}>
+      <LoginScreen />
+    </AuthContext.Provider>
+  );
 
 describe('renders ui login correctly', () => {
   it('render username input', () => {
@@ -27,5 +39,70 @@ describe('renders ui login correctly', () => {
     const element2 = getByText(/Sign up now/i);
     expect(element1).toBeTruthy();
     expect(element2).toBeTruthy();
+  });
+});
+describe('login flow', () => {
+  it('should show required username', async () => {
+    const { getByPlaceholderText, getByText } = setup();
+    const element = getByPlaceholderText('Username');
+
+    fireEvent(element, 'blur', {
+      persist: jest.fn(),
+      target: { name: '', value: '' }
+    });
+    await waitFor(() => expect(getByText('Username is required')).toBeTruthy());
+  });
+  it('should show min char username', async () => {
+    const { getByPlaceholderText, getByText } = setup();
+    const element = getByPlaceholderText('Username');
+
+    fireEvent.changeText(element, 'foo');
+    fireEvent(element, 'blur', {
+      persist: jest.fn(),
+      target: { name: '', value: '' }
+    });
+    await waitFor(() => expect(getByText('username must be at least 5 characters')).toBeTruthy());
+  });
+  it('should show required password', async () => {
+    const { getByPlaceholderText, getByText } = setup();
+    const element = getByPlaceholderText('Password');
+
+    fireEvent(element, 'blur', {
+      persist: jest.fn(),
+      target: { name: '', value: '' }
+    });
+    await waitFor(() => expect(getByText('Password is required')).toBeTruthy());
+  });
+  it('should show min char password', async () => {
+    const { getByPlaceholderText, getByText } = setup();
+    const element = getByPlaceholderText('Password');
+
+    fireEvent.changeText(element, 'foo');
+    fireEvent(element, 'blur', {
+      persist: jest.fn(),
+      target: { name: '', value: '' }
+    });
+    await waitFor(() => expect(getByText('password must be at least 5 characters')).toBeTruthy());
+  });
+  it('should enable sign in button', async () => {
+    const { getByPlaceholderText, getByA11yLabel } = setup();
+    const username = getByPlaceholderText('Username');
+    const password = getByPlaceholderText('Password');
+    const button = getByA11yLabel('btnSignIn');
+
+    fireEvent.changeText(username, 'testing');
+    fireEvent.changeText(password, 'password');
+    await waitFor(() => expect(button.props.accessibilityState.disabled).toBe(false));
+  });
+  it('should call sign in', async () => {
+    const { getByPlaceholderText, getByA11yLabel } = setup();
+    const username = getByPlaceholderText('Username');
+    const password = getByPlaceholderText('Password');
+    const button = getByA11yLabel('btnSignIn');
+
+    fireEvent.changeText(username, 'testing');
+    fireEvent.changeText(password, 'password');
+    fireEvent.press(button);
+    await waitFor(() => expect(mockContext.signIn).toHaveBeenCalledTimes(1));
   });
 });
